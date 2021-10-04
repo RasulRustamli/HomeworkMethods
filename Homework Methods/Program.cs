a337f04cd8034d299b7acff509d59c5f06a0325e
RasulRustamli <rasular@code.edu.az>
2021-10-04 16:52:57 +0400

    Proje dosyası ekle.

---
+++ Homework Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cem(25));
+            sum(25);
+        }
+        public static int cem(int a)
+        {
+            int sum = 0;
+            for(int i = 0; i <= a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+        public static void sum(int a)
+        {
+            int sum = 0;
+            for (int i = 0; i <= a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
